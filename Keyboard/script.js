9930ee7df6d17cfec7acba354efbd584378476a0
Jayasree77 <kjsree1998@gmail.com>
2020-06-24 22:02:17 +0530

    Not yet responsive

---
+++ Keyboard/script.js
@@ -0,0 +1,44 @@
+function H() {
+    var x = document.getElementById('text');
+    x.value = x.value + "H";
+}
+
+function E() {
+    var x = document.getElementById('text');
+    x.value = x.value + "E";
+}
+
+function L() {
+    var x = document.getElementById('text');
+    x.value = x.value + "L";
+}
+
+function O() {
+    var x = document.getElementById('text');
+    x.value = x.value + "O";
+}
+
+function W() {
+    var x = document.getElementById('text');
+    x.value = x.value + "W";
+}
+
+function R() {
+    var x = document.getElementById('text');
+    x.value = x.value + "R";
+}
+
+function D() {
+    var x = document.getElementById('text');
+    x.value = x.value + "D";
+}
+
+function space() {
+    var x = document.getElementById('text');
+    x.value = x.value + " ";
+}
+
+function erase() {
+    var x = document.getElementById('text');
+    x.value = x.value.slice(0, x.value.length - 1);
+}
