f17dce8304ca1cbd3811fd2e0f42cbfe4055f960
Jayasree77 <kjsree1998@gmail.com>
2020-07-16 13:05:17 +0530

    Gradient Generator

--- Background Generator/script.js
+++ Background Generator/script.js
@@ -4,24 +4,14 @@ var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var button = document.querySelector('button');
 var body = document.getElementById('gradient');
-var randomButton = false;
 
 function setGradient() {
-    if (!randomButton) {
-        body.style.background = "linear-gradient(" +
-            angle.value + "deg," +
-            color1.value + "," +
-            color2.value + ")";
-        css.textContent = body.style.background + ';';
-
-    } else {
-        body.style.background = "linear-gradient(" +
-            angle + "deg," +
-            color1 + "," +
-            color2 + ")";
-        css.textContent = body.style.background + ';';
-        randomButton = false;
-    }
+    body.style.background = "linear-gradient(" +
+        angle.value + "deg," +
+        color1.value + "," +
+        color2.value + ")";
+    console.log(color1.value);
+    css.textContent = body.style.background + ';';
 }
 color1.addEventListener("input", setGradient);
 
@@ -30,7 +20,6 @@ color2.addEventListener("input", setGradient);
 angle.addEventListener("input", setGradient);
 
 function random() {
-    randomButton = true;
     var hexValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 
     function generate(val) {
@@ -40,9 +29,9 @@ function random() {
         }
         return val
     }
-    color1 = generate('#');
-    color2 = generate("#");
-    angle = Math.floor(Math.random() * 360);
+    color1.value = generate('#');
+    color2.value = generate("#");
+    angle.value = Math.floor(Math.random() * 360);
     setGradient()
 
 }
