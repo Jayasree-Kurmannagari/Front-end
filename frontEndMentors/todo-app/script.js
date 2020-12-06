98cd1d1f870ba7a5816f708e4135026f43ea83d2
Jayasree Kurmannagari <Jayasree.Kurmannagari@ruckuswireless.com>
2020-12-06 18:48:01 +0530

    Added filters

--- frontEndMentors/todo-app/script.js
+++ frontEndMentors/todo-app/script.js
@@ -1,12 +1,31 @@
 const button = document.querySelector(".form-button");
 const input = document.querySelector(".create");
 const todoList = document.querySelector(".todo-list");
-const form = document.querySelector('.form');
-form.addEventListener('submit', addTodo);
+const form = document.querySelector(".form");
+const filterOption = document.querySelector(".filter-todo");
+const count = document.querySelector('.count');
+const refresh = document.querySelector('.refresh');
+let total = 0;
+var btns = document.querySelectorAll(".btn");
+   Array.from(btns).forEach(item => {
+      item.addEventListener("click", () => {
+         var selected = document.getElementsByClassName("active");
+         selected[0].className = selected[0].className.replace(" active", "");
+         item.className += " active";
+      });
+   });
+form.addEventListener("submit", addTodo);
 button.addEventListener("click", addTodo);
-todoList.addEventListener("click",deleteCheck);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+refresh.addEventListener("click",clear);
+
 function addTodo(event) {
   event.preventDefault();
+  if (!input.value) {
+    alert("Enter a to-do");
+    return;
+  }
   //New Div TODO
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
@@ -31,35 +50,70 @@ function addTodo(event) {
 
   todoList.appendChild(todoDiv);
   input.value = "";
- //Todo completed function
+  total++;
+  count.innerHTML = `${total} items left`;
+  //Todo completed function
   completedButton.addEventListener("click", () => {
-    if(!check || firstTime){
+    if (!check || firstTime) {
       completedButton.classList.add("hide");
-      todoDiv.classList.add('completed');
+      todoDiv.classList.add("completed");
       completedButton.innerHTML = "";
-      const image = document.createElement('img');
-       image.setAttribute('src','./images/icon-check.svg');
-    image.classList.add('check-img');
+      const image = document.createElement("img");
+      image.setAttribute("src", "./images/icon-check.svg");
+      image.classList.add("check-img");
       completedButton.appendChild(image);
-      console.log(check,firstTime);
+      total--;
+  count.innerHTML = `${total} items left`;
       check = true;
-      firstTime = false
-      }
-      else{
-        completedButton.classList.remove('hide');
-        todoDiv.classList.remove('completed');
-        console.log(check,firstTime);
-        completedButton.innerHTML = '<i class="far fa-circle"></i>';
-        check = false;
-      }
+      firstTime = false;
+    } else {
+      completedButton.classList.remove("hide");
+      todoDiv.classList.remove("completed");
+      completedButton.innerHTML = '<i class="far fa-circle"></i>';
+      total++;
+  count.innerHTML = `${total} items left`;
+      check = false;
+    }
   });
 }
 
-function deleteCheck(e){
+function deleteCheck(e) {
   const item = e.target;
-  if(item.classList[0] === 'trash-btn'){
+  if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    console.log(todo)
+    console.log(todo);
     todo.remove();
+    total--;
+    count.innerHTML = `${total} items left`;
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  console.log(todos);
+  for (let i = 1; i < todos.length; i++) {
+    switch (e.target.innerText) {
+      case "All":
+        todos[i].style.display = "flex";
+        break;
+      case "Active":
+        if(todos[i].classList.contains("completed"))
+          todos[i].style.display='none';
+        else todos[i].style.display = 'flex';
+        break;
+      case "Completed":
+        if (todos[i].classList.contains("completed"))
+          todos[i].style.display = "flex";
+        else todos[i].style.display = "none";
+    }
+  }
+}
+
+function clear(e){
+  const todos = todoList.childNodes;
+  for(let i = 1; i < todos.length; i++){
+    if (todos[i].classList.contains("completed")){
+      todos[i].remove();
+    }
   }
 }
